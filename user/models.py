604e393790a0604396d4961a31d091a960b6643b
shailaja <shailaja@tradlab.in>
2021-02-12 15:57:21 +0530

    packages requirements text file added

--- user/models.py
+++ user/models.py
@@ -7,8 +7,17 @@ class CustomUser(AbstractUser):
     system_id = models.CharField(max_length=64)
     timezone = models.CharField(max_length=120, choices=[(timezone, timezone) for timezone in pytz.common_timezones],)
 
+    def __str__(self):
+        return self.system_id + ' : ' + self.timezone
+
+    class Meta:
+        db_table = 'custom_user'
+
 
 class ActivityDetail(models.Model):
     custom_user = models.ManyToManyField(CustomUser)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'activity_detail'
