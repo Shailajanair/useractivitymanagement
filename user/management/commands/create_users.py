689936a9d7ed63406617f1e7e8d9c99a18c05af4
shailaja <shailaja@tradlab.in>
2021-02-13 15:59:49 +0530

    added some detail to readme file and also added comments to custom management command

--- user/management/commands/create_users.py
+++ user/management/commands/create_users.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for i in range(0, 10):
+            # choosing random system_id, username etc to populate database
             system_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=9))
             if CustomUser.objects.filter(system_id=system_id).exists():
                 continue
@@ -21,13 +22,15 @@ class Command(BaseCommand):
             custom_obj = CustomUser.objects.create(first_name=firstname, last_name=lastname, username=username,
                                                    system_id=system_id, timezone=time_zone)
             for j in range(0, 3):
+                # end time should always be greater than start time
                 random_year, random_month, random_date = random.randint(2000, 2021), random.randint(1, 12), random.randint(1, 25)
-                random_hour, random_hour_2, random_time, random_time_2 = random.randint(0, 12), random.randint(0, 12),\
+                random_hour, random_hour_2, random_min, random_min_2 = random.randint(0, 12), random.randint(0, 12),\
                                                                          random.randint(0, 59), random.randint(0, 59)
-                start_time_minute = min(random_time, random_time_2)
-                end_time_minute = max(random_time, random_time_2)
+                start_time_minute, end_time_minute = min(random_min, random_min_2), max(random_min, random_min_2)
                 start_time_hour, end_time_hour = min(random_hour, random_hour_2), max(random_hour, random_hour_2)
+
                 start_time, end_time = datetime(random_year, random_month, random_date, start_time_hour, start_time_minute, 00),\
                                        datetime(random_year, random_month, random_date, end_time_hour, end_time_minute, 00)
+
                 custom_obj.activitydetail_set.create(start_time=start_time, end_time=end_time)
         return
