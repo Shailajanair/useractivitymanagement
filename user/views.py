8d9dad45ae6fc761de1118dd493315ac8f37c1be
shailaja <shailaja@tradlab.in>
2021-02-13 17:00:36 +0530

    response format making similar to the given json format

--- user/views.py
+++ user/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
 from user.models import CustomUser, ActivityDetail
 from user.serializers import CustomUserSerializer, ActivityDetailSerializer
+from rest_framework.response import Response
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        serialized_data = CustomUserSerializer(self.queryset, many=True).data
+        return Response({"ok": "true", "members": serialized_data})
 
 
 class ActivityDetailViewSet(viewsets.ModelViewSet):
